1d272026081ee89b94594915cbdc07a59f61c1c6
Talha-T <talha6851@hotmail.com>
2018-01-02 17:27:55 +0300

    Export Message and Broadcast, compile

--- out/index.d.ts
+++ out/index.d.ts
@@ -1,1 +1,3 @@
 export { Twitch } from "./twitch-wrapper-ts";
+export { Message } from "./types/looseObject";
+export { Broadcaster } from "./types/looseObject";
--- out/index.js
+++ out/index.js
@@ -2,4 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var twitch_wrapper_ts_1 = require("./twitch-wrapper-ts");
 exports.Twitch = twitch_wrapper_ts_1.Twitch;
+var looseObject_1 = require("./types/looseObject");
+exports.Message = looseObject_1.Message;
+var looseObject_2 = require("./types/looseObject");
+exports.Broadcaster = looseObject_2.Broadcaster;
 //# sourceMappingURL=index.js.map
